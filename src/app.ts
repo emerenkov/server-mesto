08a5322b5c70890231b6fa41a9c1cb59aae8bcfd
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2023-02-28 14:22:14 +0400

    creat

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, {NextFunction, Request, Response} from 'express';
+import mongoose from "mongoose";
+import path from "path";
+//import routes from "./routes";
+//import {RequestCustom} from "./middleware/type";
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+mongoose.set('strictQuery', false);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   (req as RequestCustom).user = {
+//     _id: '63fdbfadff38da20690f369c' // вставьте сюда _id созданного в предыдущем пункте пользователя
+//   };
+
+//   next();
+// });
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(routes);
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
